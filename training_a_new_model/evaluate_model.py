fe2fb9e8e426c56504894d30c779295afb0c9492
Adam Geitgey <ageitgey@gmail.com>
2018-08-10 16:36:56 -0700

    Add eval demo

---
+++ training_a_new_model/evaluate_model.py
@@ -0,0 +1,45 @@
+import cv2
+import mrcnn.config
+import mrcnn.visualize
+import mrcnn.utils
+from mrcnn.model import MaskRCNN
+from pathlib import Path
+
+
+# Configuration that will be used by the Mask-RCNN library
+class ObjectDetectorConfig(mrcnn.config.Config):
+    NAME = "custom_object"
+    IMAGES_PER_GPU = 1
+    GPU_COUNT = 1
+    NUM_CLASSES = 1 + 1  # custom object + background class
+
+
+# Root directory of the project
+ROOT_DIR = Path(".")
+MODEL_DIR = ROOT_DIR / "training_logs"
+
+# Local path to trained weights file (make sure you update this)
+TRAINED_MODEL_PATH = MODEL_DIR / "custom_object20180810T1614" / "mask_rcnn_custom_object_0030.h5"
+
+# Create a Mask-RCNN model in inference mode
+model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=ObjectDetectorConfig())
+
+# Load pre-trained model
+model.load_weights(TRAINED_MODEL_PATH, by_name=True)
+
+# COCO Class names
+class_names = ['BG', 'custom_object']
+
+# Load the image we want to run detection on
+image_path = "./training_images/validation_set/object_0002.jpg"
+image = cv2.imread(image_path)
+
+# Convert the image from BGR color (which OpenCV uses) to RGB color
+rgb_image = image[:, :, ::-1]
+
+# Run the image through the model
+results = model.detect([rgb_image], verbose=1)
+
+# Visualize results
+r = results[0]
+mrcnn.visualize.display_instances(rgb_image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
